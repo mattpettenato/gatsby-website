dc0eeae57028c09c4db2921a1ff860c66cd2ef22
Matt Pettenato <mattpettenato@gmail.com>
2023-03-13 19:15:04 -0700

    portoflio'd

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,9 +9,9 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Matt Pettenato's Developer Profile`,
+    description: `Fullstack Software Developer.`,
+    author: `@mattpettenato`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
